1dbd9a9c4e5b36a85b20c79deb5207a940be1a53
steiditi <Timo.Steidinger@gmx.de>
2022-03-21 13:02:57 +0100

    Event Names

--- IPoAT-Logic/src/main/java/dhbw/ipoat/events/Event.java
+++ IPoAT-Logic/src/main/java/dhbw/ipoat/events/Event.java
@@ -6,29 +6,47 @@ import java.util.Random;
 
 public class Event {
 
-    public void doSomethingGoodOrBad(int selector, Player player) {
+    private Player player;
+
+    public Event(Player player) {
+        this.player = player;
+    }
+
+    public void doSomethingGoodOrBad(int selector) {
         switch (selector) {
             case 0: {
-                Random r = new Random();
-                int money = r.nextInt(100 - 50) + 50;
-                System.out.println("Happy Birthday! Your family sent you " + money + "€");
-                player.moneyTransactions(money);
+                executeEvent1();
                 break;
             }
             case 1: {
-                System.out.println("You had to go to the vet because on of your birds became sick. It cost you 50€");
-                player.moneyTransactions(-50);
+                executeEvent2();
                 break;
             }
             case 3: {
-                Random r = new Random();
-                int damage = r.nextInt(3 - 1) + 1;
-                System.out.println("You had a little fire in your habitat. " + damage + " nests were destroyed");
-                player.moneyTransactions(-50);
+                executeEvent3();
                 break;
             }
             default:
                 break;
         }
+    }
+
+    private void executeEvent1() {
+        Random r = new Random();
+        int money = r.nextInt(100 - 50) + 50;
+        System.out.println("Happy Birthday! Your family sent you " + money + "€");
+        player.moneyTransactions(money);
+    }
+
+    private void executeEvent2() {
+        System.out.println("You had to go to the vet because on of your birds became sick. It cost you 50€");
+        player.moneyTransactions(-50);
+    }
+
+    private void executeEvent3() {
+        Random r = new Random();
+        int damage = r.nextInt(3 - 1) + 1;
+        System.out.println("You had a little fire in your habitat. " + damage + " nests were destroyed");
+        player.moneyTransactions(-50);
     }
 }
